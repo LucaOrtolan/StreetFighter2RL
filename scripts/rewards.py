c1ec44dc425dcf62e35e671dd770faaa9645b122
emeralddawns <93622526+emeralddawns@users.noreply.github.com>
2026-03-15 14:26:48 +0000

    exp 1: x**3.... 4 is too steep

--- scripts/rewards.py
+++ scripts/rewards.py
@@ -9,7 +9,7 @@ def get_rewards(dense_coeff : float, aggresive_coeff: float, damage_taken: float
     else:
         eps = 10**-6
         # EXP: 1 - Minimal Penalty for lower amounts of damage
-        tuned_damage_taken = ((max_damage_taken/(damage_taken + eps))**4) * damage_taken
+        tuned_damage_taken = ((max_damage_taken/(damage_taken + eps))**3) * damage_taken
         custom_reward = dense_coeff * (aggresive_coeff * damage_dealt - tuned_damage_taken)
 
     return custom_reward
